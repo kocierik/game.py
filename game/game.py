6f3e707d73ef44b4fd0ddfc82d686084c68409b1
kocierik <41591336+kocierik@users.noreply.github.com>
2020-03-22 20:41:33 +0100

    python game
    MVP game

---
+++ game/game.py
@@ -0,0 +1,136 @@
+import pygame
+import math
+pygame.init()
+
+screenwidth = 500
+screenheight = 480
+window = pygame.display.set_mode((screenwidth, screenheight))
+pygame.display.set_caption('hi im a game!')
+
+
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load(
+    'R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load(
+    'L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+clock = pygame.time.Clock()
+
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.standing = True
+
+    def draw(self, window):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if not(self.standing):
+            if self.left:
+                window.blit(walkLeft[self.walkCount//3], (self.x, self.y))
+                self.walkCount = self.walkCount + 1
+            elif self.right:
+                window.blit(walkRight[self.walkCount//3], (self.x, self.y))
+                self.walkCount = self.walkCount + 1
+        else:
+            if self.right:
+                window.blit(walkRight[0], (self.x, self.y))
+            else:
+                window.blit(walkLeft[0], (self.x, self.y))
+
+
+class projectile(object):
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, window):
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
+
+
+def game_update():
+    window.blit(bg, (0, 0))
+    man.draw(window)
+    for bullet in bullets:
+        bullet.draw(window)
+
+    pygame.display.update()
+
+# loop del gioco
+
+
+man = player(300, 410, 64, 64)
+bullets = []
+run = True
+while run:
+    clock.tick(27)
+    # pygame.time.delay(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    for bullet in bullets:
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x = bullet.x + bullet.vel
+        else:
+            bullets.pop(bullets.index(bullet))  # pop rimuove un elemento
+            
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(projectile(round(man.x + man.width // 2),
+                                 round(man.y + man.height // 2), 6, (255, 0, 0), facing))
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x = man.x - man.vel
+        man.left = True
+        man.right = False
+        man.standing = False
+    elif keys[pygame.K_RIGHT] and man.x < screenwidth - man.width - man.vel:
+        man.x = man.x + man.vel
+        man.left = False
+        man.right = True
+        man.standing = False
+    else:
+        man.standing = True
+        man.walkCount = 0
+
+    if not (man.isJump):
+        if keys[pygame.K_UP]:
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
+    else:
+        if man.jumpCount >= -10:
+            neg = 1
+            if man.jumpCount < 0:
+                neg = -1
+            man.y = man.y - (man.jumpCount ** 2) * 0.5 * neg
+            man.jumpCount = man.jumpCount - 1
+        else:
+            man.isJump = False
+            man.jumpCount = 10
+
+    game_update()
+
+pygame.quit()
